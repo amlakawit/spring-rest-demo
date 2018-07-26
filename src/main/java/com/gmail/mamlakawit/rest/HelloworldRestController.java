54d633a3e0f7274f29a5a366087b78a39c4ab8da
amlakawit <mamlakawit@gmail.com>
2018-07-25 23:12:57 -0400

    retrun students by id using @PathVariables

--- src/main/java/com/gmail/mamlakawit/rest/HelloworldRestController.java
+++ src/main/java/com/gmail/mamlakawit/rest/HelloworldRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RestControllerDemo {
+public class HelloworldRestController {
 	
 	@GetMapping("/helloworld") //REST endpoint 
 	public String HelloWorld(){
--- src/main/java/com/gmail/mamlakawit/rest/StudentRestController.java
+++ src/main/java/com/gmail/mamlakawit/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.gmail.mamlakawit.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,18 +15,31 @@ import com.gmail.mamlakawit.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-	
-	//define endpoint for "/students" -return list of students
-	@GetMapping("/students")
-	public List<Student> getStudents(){
-		
-		List<Student> students = new ArrayList<>();
-		
+
+	private List<Student> students;
+
+	// use @PostConstruct to lead student data
+	@PostConstruct
+	public void loadStudentData() {
+		students = new ArrayList<>();
 		students.add(new Student("Ayele", "Bayu"));
 		students.add(new Student("Eliana", "Biruk"));
-		students.add(new Student("Hibist", "Mena"));		
-		
-		return students;
-		
+		students.add(new Student("Hibist", "Mena"));
+
 	}
+
+	// define endpoint for "/students" -return list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		return students;
+
+	}
+
+	// define endpoint for "/students/{id}" - get student at the given index in
+	// the list
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) { 
+		return students.get(studentId);
+	}
+
 }
